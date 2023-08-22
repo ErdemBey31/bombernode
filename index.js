04169cd56820ad9855f517afe493c6ec20339270
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-22 18:35:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const { Telegraf } = require('telegraf');
 const { performance } = require('perf_hooks');
 const bot = new Telegraf('6357522659:AAFUp0jzOcLBNiNBPGON-SO6srVMC_dqpkw');
-const http = require('http')
+const http = require('http');
 const fs = require('fs');
-
-
-
 
 bot.command('ban', (ctx) => {
   const bannedUserId = ctx.message.reply_to_message.from.id;
@@ -20,9 +17,6 @@ bot.command('ban', (ctx) => {
     }
   });
 });
-
-
-
 
 bot.command("start", (ctx) => {
   const bannedIds = fs.readFileSync('banneds.txt', 'utf-8').split('\n');
@@ -42,6 +36,7 @@ bot.command("start", (ctx) => {
     }
   });
 });
+
 bot.action('ping', (ctx) => {
     const start = performance.now();
     const chatId = ctx.chat.id;
@@ -49,34 +44,37 @@ bot.action('ping', (ctx) => {
     ctx.replyWithMarkdown("*Ping ölçülüyor...*")
     const end = performance.now();
     const pingTime = Math.round(end - start);
-    const alertText = `Ping değeri:. ${pingTime}`;
-    return ctx.answerCbQuery({text:alertText, show_alert: true});
+    const alertText = `Ping değeri: ${pingTime}`;
+    ctx.answerCbQuery({ text: alertText, show_alert: true });
     ctx.deleteMessage();
 });
-//https://oslocheck.com.tr/api/smsbomber?key=ggsahip&numara=${numara}&miktar=${miktar}
 
 bot.command("bomb", (ctx, match) => {
     const bannedIds = fs.readFileSync('banneds.txt', 'utf-8').split('\n');
+    const userId = ctx.message.from.id;
+
     if (bannedIds.includes(userId.toString())) {
       console.log('Kullanıcı yasaklandı:', userId);
       return ctx.reply('Üzgünüz, yasaklandınız!');
-      return;
-    const numara = match[1]
-    const miktar = match[2]
-    http.get(`https://oslocheck.com.tr/api/smsbomber?key=ggsahip&numara=${numara}&miktar=${miktar.toString()}`, (res) => {
+    }
+
+    const numara = match[1];
+    const miktar = match[2];
+
+    http.get(
+      `https://oslocheck.com.tr/api/smsbomber?key=ggsahip&numara=${numara}&miktar=${miktar.toString()}`,
+      (res) => {
         let responseData = '';
 
         res.on('data', (chunk) => {
-            responseData += chunk;
-  });
+          responseData += chunk;
+        });
 
         res.on('end', () => {
-            console.log('Alınan metin:', responseData);
-            ctx.replyWithHtml(`<b>Sonuç:</b> <code> ${responseData} </code>`)
+          console.log('Alınan metin:', responseData);
+          ctx.replyWithHtml(`<b>Sonuç:</b> <code> ${responseData} </code>`);
+        });
+      });
 });
 
-
 bot.launch();
-
-
-
